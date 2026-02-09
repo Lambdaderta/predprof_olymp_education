b18923ba36725a50df1eead5d4fd24a716740175
Lambdaderta <chto.nado.2222@gmail.com>
2026-02-09 03:04:15 +0300

    Настроенные докер файлы, правка конфигов и тд.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -83,7 +83,8 @@ class Settings(BaseSettings):
         default_factory=lambda: [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ],
         
         description="CORS origins"
